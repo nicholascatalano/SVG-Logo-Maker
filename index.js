b82193d55ee8bcd35c6b69d4fd75c319a3dfcf46
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-18 18:44:49 -0500

    added first two questions to promptUser questions array

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const {} = require("./lib/shapes");
+
+// HELPER FUNCTIONS
+// prompt function to prompt user of questions when node index is ran
+function promptUser() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message:
+        "Please enter the text you would like to display (UP TO 3 CHARACTERS).",
+      name: "textInput",
+    },
+    {
+      type: "input",
+      message:
+        "Please enter the text color you'd like to use (OR a hexadecimal number).",
+      name: "textColor",
+    },
+    {},
+  ]);
+}
+
+// INITIALIZATION
