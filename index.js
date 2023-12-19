092314c0d08128572a0665e2d3ddc67da2eab6f4
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-18 19:10:07 -0500

    added .then callback to promptUser function

--- index.js
+++ index.js
@@ -6,32 +6,42 @@ const {} = require("./lib/shapes");
 // HELPER FUNCTIONS
 // prompt function to prompt user of questions when node index is ran
 function promptUser() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message:
-        "Please enter the text you would like to display (UP TO 3 CHARACTERS).",
-      name: "textInput",
-    },
-    {
-      type: "input",
-      message:
-        "Please enter the text color you would like to use (OR a hexadecimal number).",
-      name: "textColor",
-    },
-    {
-      type: "list",
-      message: "Please choose which shape you would like to display.",
-      name: "shape",
-      choices: ["Circle", "Triangle", "Square"],
-    },
-    {
-      type: "input",
-      message:
-        "Please choose the color of the shape (OR a hexadecimal number).",
-      name: "shapeColor",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "Please enter the text you would like to display (UP TO 3 CHARACTERS).",
+        name: "textInput",
+      },
+      {
+        type: "input",
+        message:
+          "Please enter the text color you would like to use (OR a hexadecimal number).",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "Please choose which shape you would like to display.",
+        name: "shape",
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        type: "input",
+        message:
+          "Please choose the color of the shape (OR a hexadecimal number).",
+        name: "shapeColor",
+      },
+    ])
+    .then((answers) => {
+      if (answers.textInput.length > 3 || !answers.textInput) {
+        console.log("Enter a text value of at least 3 characters.");
+        promptUser();
+      } else {
+        console.log("File written!");
+      }
+    });
 }
 
 // INITIALIZATION
+promptUser();
