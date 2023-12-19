0a20b2823c80964c95f0933db7784632c4e12bc4
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-18 21:26:10 -0500

    added basic svg content code to write to file function

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ function writeToFile() {
 
   // sets file content to empty string
   let svgContent = "";
+  // sets file content height and width
+  svgContent = `<svg version="1.1"
+  width="300" height="200"
+  xmlns="http://www.w3.org/2000/svg">`;
+
+  // inserts user shape choice into file
+  svgContent += `${answers.shape}`;
 }
 
 // INITIALIZATION
