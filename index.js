29647a8de96d7a54138312aff89f91ab6b10a07c
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-18 20:12:05 -0500

    added .then following prompts to be able to access answers, and added callback writeToFile function to run using answers, creating a file called logo.svg

--- index.js
+++ index.js
@@ -6,47 +6,51 @@ const {} = require("./lib/shapes");
 // HELPER FUNCTIONS
 // prompt function to prompt user of questions when node index is ran
 function promptUser() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message:
-        "Please enter the text you would like to display on the shape (UP TO 3 CHARACTERS).",
-      name: "textInput",
-      // validates if textInput is > 3 char or blank, if so, reruns function and logs the issue, else, continues"
-      validate: (textInput) => {
-        if (textInput.length > 3) {
-          return console.log("\n", "Enter a text value of up to 3 characters.");
-        }
-        return true;
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "Please enter the text you would like to display on the shape (UP TO 3 CHARACTERS).",
+        name: "textInput",
+        // validates if textInput is > 3 char, if so, returns true to continue prompts"
+        validate: (textInput) => {
+          if (textInput.length > 3) {
+            return console.log(
+              "\n",
+              "Enter a text value of up to 3 characters."
+            );
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message:
-        "Please enter the text color you would like to use (OR a hexadecimal number).",
-      name: "textColor",
-    },
-    {
-      type: "list",
-      message: "Please choose which shape you would like to display.",
-      name: "shape",
-      choices: ["Circle", "Triangle", "Square"],
-    },
-    {
-      type: "input",
-      message:
-        "Please choose the color of the shape (OR a hexadecimal number).",
-      name: "shapeColor",
-    },
-  ]);
-  // .then((answers) => {
-  //   if (answers.textInput.length > 3 || !answers.textInput) {
-  //     console.log("Enter a text value of at least 3 characters.");
-  //     promptUser();
-  //   } else {
-  //     console.log("File written!");
-  //   }
-  // });
+      {
+        type: "input",
+        message:
+          "Please enter the text color you would like to use (OR a hexadecimal number).",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "Please choose which shape you would like to display.",
+        name: "shape",
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        type: "input",
+        message:
+          "Please choose the color of the shape (OR a hexadecimal number).",
+        name: "shapeColor",
+      },
+    ])
+    // gathers answers from user and runs file write function with the file name logo.svg
+    .then((answers) => {
+      writeToFile("logo.svg", answers);
+    });
+}
+
+function writeToFile() {
+  console.log("File written!");
 }
 
 // INITIALIZATION
